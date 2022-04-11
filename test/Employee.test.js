94b71ca4f7d7c0139676e6f453629f5ee7f53b33
Matthew Carson <matthew.carson@pm.me>
2022-04-10 22:05:08 -0400

    Add getters to Employee class

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,5 +1,7 @@
 const Employee = require('../lib/Employee.js')
 
+const employee = new Employee('Jane Test', 12, 'jane.test@example.com')
+
 describe('Employee', () => {
   describe('Initialization', () => {
     it('should be initialized with a name, id, and email attribute', () => {
@@ -24,6 +26,26 @@ describe('Employee', () => {
       
       expect(() => new Employee('John Test', 12, '')).toThrow(new Error('A string email is required.'))
       expect(() => new Employee('John Test', 12, 24)).toThrow(new Error('A string email is required.'))
+    })
+  })
+  describe('getId', () => {
+    it('should return the employee\'s id', () => {
+      expect(employee.getId()).toEqual(12)
+    })
+  })
+  describe('getName', () => {
+    it('should return the employee\'s name', () => {
+      expect(employee.getName()).toEqual('Jane Test')
+    })
+  })
+  describe('getEmail', () => {
+    it('should return the employee\'s email address', () => {
+      expect(employee.getEmail()).toEqual('jane.test@example.com')
+    })
+  })
+  describe('getRole', () => {
+    it('should return a role of \'Employee\'', () => {
+      expect(employee.getRole()).toEqual('Employee')
     })
   })
 })
